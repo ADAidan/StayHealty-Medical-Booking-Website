73bc02797566a038f300a94cab1d12aafd0b6f73
ADAidan <ADyer2190@gmail.com>
2024-01-25 03:25:36 -0500

    Connected the sign up and login forms to MongoDB

--- src/components/Sign_Up/Sign_Up.js
+++ src/components/Sign_Up/Sign_Up.js
@@ -1,65 +1,112 @@
 import React, { useState } from 'react';
-import './Sign_Up.css';
+import './Sign_Up.css'
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 
-const SignUp = () => {
+const Sign_Up = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
-    const [phoneError, setPhoneError] = useState(false);
+    const [password, setPassword] = useState('');
+    const [showerr, setShowerr] = useState('');
 
-    const handleSubmit = (e) => {
+    const navigate = useNavigate();
+
+    const register = async (e) => {
         e.preventDefault();
-        console.log('Submitted form')
+        let json;
 
-        // check if phone number is valid
-        if (phone.length > 10) {
-            setPhoneError(true);
-        } else {
-            setPhoneError(false)
+        console.log("Submitted form");
+        console.log("Name: ", name);
+        console.log("Email: ", email);
+        console.log("Phone: ", phone);
+        console.log("Password: ", password);
+        console.log("Error: ", showerr);
+
+        try {
+            // API Call
+            const response = await fetch(`${API_URL}/api/auth/register`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    password: password,
+                    phone: phone,
+                }),
+            });
+
+            if (!response.ok) {
+                json = await response.json();
+                console.log('Error:', json);
+                setShowerr(json.error);
+            } else {
+                json = await response.json();
+                console.log("Success:", json);
+            }
+
+            if (json.authtoken) {
+                sessionStorage.setItem("auth-token", json.authtoken);
+                sessionStorage.setItem("name", name);
+                // phone and email
+                sessionStorage.setItem("phone", phone);
+                sessionStorage.setItem("email", email);
+                // Redirect to home page
+                console.log("creating account")
+                navigate("/");   //on directing to home page you need to give logic to change login and signup buttons with name of the user and logout button where you have implemented Navbar functionality
+                window.location.reload();
+                console.log("account created")
+            } else {
+                if (json.errors) {
+                    for (const error of json.errors) {
+                        setShowerr(error.msg);
+                    }
+                } else {
+                    setShowerr(json.error);
+                }
+            }
+
+            console.log(json);
+        } catch (error) {
+            console.error("Error:", error);
+            setShowerr("An error occurred. Please try again later.");
         }
-
-        console.log('Phone: ', phone)
-        console.log('Phone Error: ', phoneError)
-
-        // Submit the form
     };
 
     return (
-        <div className="container" style={{ marginTop: '5%' }}>
-            <div className="signup-grid">
-                <div className="signup-text">
-                    <h1>Sign Up</h1>
-                </div>
-                <div className="signup-text1" style={{ textAlign: 'left' }}>
-                    Already a member? <span><a href="../Login/Login.html" style={{ color: '#2190FF' }}> Login</a></span>
-                </div>
-                <div className="signup-form">
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" name="name" id="name" required className="form-control" placeholder="Enter your name" aria-describedby="helpId" />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="phone">Phone</label>
-                            <input type="tel" name="phone" id="phone" required className={`${phoneError ? 'form-error' : 'form-control form-error'}`} placeholder="Enter your phone number" aria-describedby="helpId" value={phone} onChange={(e) => setPhone(e.target.value)} />
-                            {phoneError && <p className="error-message">Phone number must be 10 or fewer digits.</p>}
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" required className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" id="password" required className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
-                        </div>
-                        <div className="btn-group">
-                            <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Submit</button>
-                            <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
-                        </div>
-                    </form>
-                </div>
+        <div className="container" style={{marginTop:'10%'}}>
+        <div className="signup-grid">
+        <div className="signup-form">
+         <form method="POST" onSubmit={register}>
+            <div className="form-group">
+                <label htmlFor="email">Email</label>
+                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
+                 {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
             </div>
+            <div className="form-group">
+                <label htmlFor="phone">Phone</label>
+                    <input value={phone} onChange={(e) => setPhone(e.target.value)} type="text" name="phone" id="phone" className="form-control" placeholder="Enter your phone" aria-describedby="helpId" />
+                    {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
+            </div>
+            <div className="form-group">
+                <label htmlFor="name">Name</label>
+                    <input value={name} onChange={(e) => setName(e.target.value)} type="text" name="name" id="name" className="form-control" placeholder="Enter your name" aria-describedby="helpId" />
+                    {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
+                    {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
+            </div>
+            <button type="submit" className="btn btn-primary">Sign Up</button>
+         </form>
+         </div>
+         </div>
         </div>
+ //Sign up role is not stored in database. You can apply logic for this according to your react code.
     );
-};
+}
 
-export default SignUp;
+export default Sign_Up;
