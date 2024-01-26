cc092be577bba91c3cec205efb8094e808684647
ADAidan <ADyer2190@gmail.com>
2024-01-26 18:01:51 -0500

    Integrated the consultation component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import LandingPage from './components/Landing_Page/LandingPage';
 import SignUp from './components/Sign_Up/Sign_Up';
 import Login from './components/Login/Login';
+import InstantConsultation from './components/InstantConsultationBooking/InstantConsultation';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
                   <Route path="/" element={<LandingPage/>} />
                   <Route path="/signup" element={<SignUp/>} />
                   <Route path="/login" element={<Login/>} />
+                  <Route path="/instant-consultation" element={<InstantConsultation/>} />
               </Routes>
             
         </BrowserRouter>
